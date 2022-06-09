567b734c83235fcfa4e06bfac6090b641f02ad57
connor-kfitz <connorkfitzsimmons@gmail.com>
2022-06-09 17:30:14 -0400

    Added table of contents

---
+++ NEWREADME.md
@@ -0,0 +1,30 @@
+
+# a
+## Table of Contents
+1. [Description](#Description)
+2. [Installation](#Installation)
+3. [Usage](#Usage)
+4. [Contribution](#Contribution)
+5. [Tests](#Tests)
+6. [Questions](#Questions)
+7. [License](#License)
+
+<a name="description"></a>
+## Description 
+a
+## Installation
+a
+## Usage
+a
+## Contributing
+a
+## Tests
+a
+
+## Questions
+For any questions, please contact me at the email provided below:
+* Email:  a
+* Github:  a
+
+## License
+This README is under the following license:  None
--- index.js
+++ index.js
@@ -1,23 +1,15 @@
-// TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const fs =require('fs');
-const { title } = require('process');
 const filename = `NEWREADME.md`;
 
-// TODO: Create an array of questions for user input
 var questions = ['What is the Title?', 'Please provide a description','Please provide installation instructions', 'Please provide usage information', 'Please provide contribution guidelines', 'Please provide test instructions','What is your email address','What is your Github link', 'Which license would you like to include?'];
-yessir = 'Hello'
-
-// console.log(questions);
-// TODO: Create a function to write README file
 
 function writeToFile(data) {
     fs.writeFile(filename, data, (err) =>
-      err ? console.log(err) : console.log(null)
+      err ? console.log(err) : console.log('Congratulations, your README as successfully created')
     );
 }
 
-// TODO: Create a function to initialize app
 function init() {
   inquirer
   .prompt([
@@ -62,7 +54,7 @@ function init() {
   message: questions[7],
   },
   {
-  type: 'checkbox',
+  type: 'list',
   message: questions[8],
   name: 'license',
   choices: ['MIT', 'APACHE 2.0', 'GPL 3.0', 'BSD 3', 'None']
@@ -70,29 +62,37 @@ function init() {
   ])
 
   .then((data) => {
-    console.log(data.title);
 
-    var licenseSelection = '';
     if(data.license == 'MIT'){
-      licenseBadge = '![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)'
+      licenseBadge = '![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)';
     }
     else if(data.license == 'APACHE 2.0'){
-      licenseBadge = '![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)'
+      licenseBadge = '![License: MIT](https://img.shields.io/badge/License-Apache_2.0-blue.svg)';
     }
     else if(data.license == 'GPL 3.0'){
-      licenseBadge = '![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)'
+      licenseBadge = '![License: MIT](https://img.shields.io/badge/License-GPLv3-blue.svg)';
     }
     else if(data.license == 'BSD 3'){
-      licenseBadge = '![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)'
+      licenseBadge = '![License: MIT](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)';
     }
     else{
-      licenseBadge = '![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)'
+      licenseBadge = '';
     }
 
     var skel = 
 `${licenseBadge}
 # ${data.title}
-## Description
+## Table of Contents
+1. [Description](#Description)
+2. [Installation](#Installation)
+3. [Usage](#Usage)
+4. [Contribution](#Contribution)
+5. [Tests](#Tests)
+6. [Questions](#Questions)
+7. [License](#License)
+
+<a name="description"></a>
+## Description 
 ${data.description}
 ## Installation
 ${data.installation}
@@ -105,24 +105,14 @@ ${data.test}
 
 ## Questions
 For any questions, please contact me at the email provided below:
-Email:  ${data.email}
-Github:  ${data.github}
+* Email:  ${data.email}
+* Github:  ${data.github}
 
 ## License
 This README is under the following license:  ${data.license}
-
-
 `
     writeToFile(skel);
   });
 }
 
-// Function call to initialize app
 init();
-
-// What kind of license should your project have?
-// MIT
-// APACHE 2.0
-// GPL 3.0
-// BSD 3
-// None
